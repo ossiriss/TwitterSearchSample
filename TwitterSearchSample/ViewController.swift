4fa1804f495baa8c2eee818e953f2e35abf3de6d
Boris Estrin <borisestrin@gmail.com>
2017-02-15 15:48:56 +0200

    table view shows tweets

--- TwitterSearchSample/ViewController.swift
+++ TwitterSearchSample/ViewController.swift
@@ -10,6 +10,23 @@ import UIKit
 import TwitterSearch
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var searchString: UITextField!
+    
+    @IBAction func showButton(_ sender: Any) {
+        guard searchString.text != nil && searchString.text?.trimmingCharacters(in: .whitespaces) != "" else {
+            let alert = UIAlertController(title: "Error", message: "You cant serch for an empty string", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Ok", style: .cancel , handler: nil))
+            present(alert, animated: true, completion: nil)
+            return
+        }
+        
+        TwitterSearch.Api.getTweetsArray(searchPhrase: searchString.text!){status,tweetsArray,error in
+            if status{
+                self.performSegue(withIdentifier: "showTableView", sender: tweetsArray)
+            }
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,6 +38,10 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let vc = segue.destination as! TweetsTableViewController
+        vc.tweets = sender as! [String]!
+    }
 
 }
 
